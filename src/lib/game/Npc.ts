761d69d93c51e1aec4cf4aad891f74f97a1e59c7
em-d3v <em_dev_9836@outlook.com>
2024-06-02 20:09:44 -0500

    added more files

---
+++ src/lib/game/Npc.ts
@@ -0,0 +1,11 @@
+import Entity from "./Entity";
+
+class Npc extends Entity{
+    render(): any {
+    }
+
+    update(): any {
+    }
+
+}
+export default Npc;
---
+++ src/lib/game/Player.ts
@@ -0,0 +1,18 @@
+import Entity from "./Entity";
+
+class Player extends Entity{
+    constructor() {
+        super();
+    }
+    render(): any {
+    }
+
+    update(): any {
+    }
+    teleport(pos:{}):void{
+
+    }
+
+}
+
+export default Player;
