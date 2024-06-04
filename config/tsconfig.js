5c9dfc86fb532c32a756b815be69a9c7c138aea0
em-d3v <em_dev_9836@outlook.com>
2024-06-04 16:30:44 -0500

    modified config tsconfigs

--- config/tsconfig.js
+++ config/tsconfig.js
@@ -1,9 +1,25 @@
+
 /**
  *
  * typescript configurations
  */
-let tsconfig = {
 
+
+let tsconfig = {
+    compilerOptions:{
+        module: "ES6",
+        strict: true,
+        baseUrl: "",
+        rootDir: "/src/lib",
+        outDir: "dist",
+        sourceMap: true,
+        declaration: false,
+        declarationDir: "/types",
+        declarationMap: false,
+        preserveConstEnums: true
+    },
+    include: ["src/lib/*"],
+    exclude: ["src/game"]
 }
 
 export default tsconfig;
